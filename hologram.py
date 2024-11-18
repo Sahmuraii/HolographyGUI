22cb15cb29da8f294ce8baabbeace4d1ed278877
1001-Veric-William <91084502+Sahmuraii@users.noreply.github.com>
2024-11-18 13:35:46 -0800

    Working Red Holographic

--- hologram.py
+++ hologram.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import numpy as np
+from FresnelPropagator import FresnelPropagator
+import matplotlib.pyplot as plt
 
 def crop_to_square(image):
     width, height = image.size
@@ -34,3 +36,10 @@ def generate_contrast_image(ref_image, raw_image):
 
     # Save the contrast image
     return contrast_image
+
+def generate_red_hologram(contrast, reconstruction_distance, wavelength, pixel_size):
+    contrast_red = np.array(contrast.split()[0])
+    viewRed = -np.abs(FresnelPropagator(contrast_red, pixel_size, wavelength, reconstruction_distance))
+    view_red_image = Image.fromarray(viewRed.astype(np.uint8))
+    #plt.imshow(view_red_image, cmap='Reds')
+    return view_red_image
--- hologramGUI.py
+++ hologramGUI.py
@@ -1,15 +1,19 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
-from hologram import generate_contrast_image
+from hologram import generate_contrast_image, generate_red_hologram
 
 class HologramGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Hologram GUI")
+        self.reconstruction_distance = 1000
+        self.wavelength = 0.0000006328
+        self.pixel_size = 3.45
 
         self.raw_image = None
         self.reference_image = None
+        self.contrast_image = None
 
         # Create a frame to hold the buttons
         button_frame = tk.Frame(root)
@@ -24,6 +28,9 @@ class HologramGUI:
 
         self.generate_contrast_button = tk.Button(button_frame, text="Generate Contrast Image", command=self.generate_contrast_image)
         self.generate_contrast_button.pack(side=tk.LEFT, padx=5)
+
+        self.open_red_hologram_window = tk.Button(button_frame, text="Open Red Hologram Window", command=self.open_red_hologram_window)
+        self.open_red_hologram_window.pack(side=tk.LEFT, padx=5)
 
 
         self.raw_image_label = tk.Label(root)
@@ -52,7 +59,6 @@ class HologramGUI:
             tk.messagebox.showerror("Error", "No image selected")
 
     def display_image(self, image, label):
-        image.thumbnail((200, 200))
         photo = ImageTk.PhotoImage(image)
         label.config(image=photo)
         label.image = photo
@@ -60,8 +66,32 @@ class HologramGUI:
     def generate_contrast_image(self):
         if self.raw_image and self.reference_image:
             contrast_image = generate_contrast_image(self.raw_image, self.reference_image)
-            self.display_image(contrast_image, self.contrast_image_label)
+            self.contrast_image = contrast_image
+            display_contrast = contrast_image.resize((500,500))
+            self.display_image(display_contrast, self.contrast_image_label)
+
+    def open_red_hologram_window(self):
+        new_window = tk.Toplevel(self.root)
+        new_window.title("Red Hologram")
         
+        # Add a label to display the red hologram
+        self.red_hologram_label = tk.Label(new_window)
+        self.red_hologram_label.pack()
+        
+        # Add a button to generate the red hologram
+        generate_red_hologram_button = tk.Button(new_window, text="Generate Red Hologram", command=lambda: self.generate_red_hologram(new_window))
+        generate_red_hologram_button.pack(pady=10)
+
+    def generate_red_hologram(self, window):
+        if self.contrast_image:
+            red_hologram = generate_red_hologram(self.contrast_image, self.reconstruction_distance, self.wavelength, self.pixel_size)
+            display_red = red_hologram.resize((500,500))
+            photo = ImageTk.PhotoImage(display_red)
+        
+            # Update the label in the new window with the image
+            self.red_hologram_label.config(image=photo)
+            self.red_hologram_label.image = photo
+
 
 if __name__ == "__main__":
     root = tk.Tk()
